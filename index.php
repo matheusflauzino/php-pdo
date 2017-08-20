130b8b7fbe393411c6ec89e91b6ec955918496e2
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2017-08-20 20:37:43 -0300

    FetchAll

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 try {
 	$conn = new \PDO("mysql:host=localhost;dbname=test_oo", "root", "root");
+
+	$query = "select * from products";
+	//$query = "insert into `products` (`name`, `desc`) values ('eBook','Learn Javascript')";
+	//$ret = $conn->exec($query);
+	$stmt = $conn->query($query);
+	$list = $stmt->fetchAll();
+
+	echo $list[0][1];
+
+	//print_r($list);
+
 } catch (\PDOException $e) {
 	echo 'Erro! Message: ' . $e->getMessage() . ' Code: ' . $e->getCode();
 }
