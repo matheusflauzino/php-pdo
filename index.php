03103706905237dec084257148617f6e345815b7
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2017-08-20 21:22:15 -0300

    inject

--- index.php
+++ index.php
@@ -2,15 +2,30 @@
 try {
 	$conn = new \PDO("mysql:host=localhost;dbname=test_oo", "root", "root");
 
-	$query = "select * from products";
+	$query = "select * from products where id = {$_GET['id']}";
 	//$query = "insert into `products` (`name`, `desc`) values ('eBook','Learn Javascript')";
 	//$ret = $conn->exec($query);
-	$stmt = $conn->query($query);
-	$list = $stmt->fetchAll();
 
-	echo $list[0][1];
+	//$stmt = $conn->query($query);
+	//$list = $stmt->fetchAll();
+
+	//$list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	//$list = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 	//print_r($list);
+
+	//echo $list[0]->name;
+
+	//$list = $stmt->fetch(PDO::FETCH_OBJ);
+
+	//print_r($list);
+
+	//echo $list->name;
+
+	foreach ($conn->query($query) as $product) {
+		echo $product['id'] . ' ' . $product['name'];
+		echo '<br />';
+	}
 
 } catch (\PDOException $e) {
 	echo 'Erro! Message: ' . $e->getMessage() . ' Code: ' . $e->getCode();
